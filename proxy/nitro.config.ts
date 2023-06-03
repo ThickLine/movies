7b68c5b3958023a3d62ce4e89ef607b761211196
ThickLine <31936990+ThickLine@users.noreply.github.com>
2023-06-03 22:34:07 +0300

    links

--- proxy/nitro.config.ts
+++ proxy/nitro.config.ts
@@ -2,12 +2,17 @@ import { defineNitroConfig } from 'nitropack'
 
 export default defineNitroConfig({
   routeRules: {
-    '/**': { cors: true },
-    '/tmdb/**': { swr: 3600 },
+    "/**": { cors: true },
+    "/tmdb/**": { swr: 3600 },
+    "/api/v1/**": {
+      cors: true,
+      headers: { "access-control-allow-methods": "GET" },
+    },
+    "/proxy/**": { proxy: "/api/**" },
   },
   runtimeConfig: {
     tmdb: {
-      apiKey: process.env.TMDB_API_KEY || '',
+      apiKey: process.env.TMDB_API_KEY || "",
     },
   },
-})
+});
